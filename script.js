9341efc0c59a32107543bd2a5c0a08d1d458a07c
alexandrr2222 <237439209+alexandrr2222@users.noreply.github.com>
2026-01-26 16:28:43 +0200

    Fix bugs

--- script.js
+++ script.js
@@ -13,45 +13,88 @@ function divide(x, y){
 function squareRoot(x){
     return Math.sqrt(x);
 }
+function exponent(x, y){
+    return x ** y;
+}
+function modulo(x, y){
+    return x % y;
+}
+function removeZeros(result){
+    for(let i = 0; rounder >= i; i++){
+        let checker = result[result.length - 1];
+        if(checker === "."){
+            result = result.slice(0, result.length - 1);
+            break;
+        }
+        else if(checker === "0"){
+            result = result.slice(0, result.length - 1);
+        }
+        else{
+            break;
+        }
+    }
+    return result;
+}
 function operate(opr, numA, numB){
     if(opr==="+"){
         if(add(numA, numB) % 1 === 0){
             return add(numA, numB);
         }
-        else return Number.parseFloat(add(numA, numB)).toFixed(2)
+        else return Number.parseFloat(add(numA, numB)).toFixed(rounder)
     }
     else if(opr==="-"){
         if(subtract(numA, numB) % 1 === 0){
             return subtract(numA, numB);
         }
-        else return Number.parseFloat(subtract(numA, numB)).toFixed(2)
+        else return Number.parseFloat(subtract(numA, numB)).toFixed(rounder)
     }
     else if(opr==="*"){
         if(multiply(numA, numB) % 1 === 0){
             return multiply(numA, numB);
         }
-        else return Number.parseFloat(multiply(numA, numB)).toFixed(2)
+        else return Number.parseFloat(multiply(numA, numB)).toFixed(rounder)
     }
-    else if(opr==="/"){
+    else if(opr==="÷"){
         if(divide(numA, numB) % 1 === 0){
             return divide(numA, numB);
         }
-        else return Number.parseFloat(divide(numA, numB)).toFixed(2)
+        else return Number.parseFloat(divide(numA, numB)).toFixed(rounder)
     }
     else if(opr==="√"){
-        return Number.parseFloat(squareRoot(numB)).toFixed(2)
+        if(squareRoot(numB) % 1 === 0){
+            return squareRoot(numB);
+        }
+        else return Number.parseFloat(squareRoot(numB)).toFixed(rounder)
+    }
+    else if(opr==="^"){
+        if(exponent(numA, numB) % 1 === 0){
+            return exponent(numA, numB);
+        }
+        else return Number.parseFloat(exponent(numA, numB)).toFixed(rounder)
+    }
+    else if(opr==="mod"){
+        if(modulo(numA, numB) % 1 === 0){
+            return modulo(numA, numB);
+        }
+        else return Number.parseFloat(modulo(numA, numB)).toFixed(rounder)
     }
 }
 
 const calcNumbers = document.querySelectorAll(".calcNumber");
 const calcOperators = document.querySelectorAll(".calcOperator");
-const screenNumber = document.querySelector("#screen");
+const screenNumber = document.querySelector("#screenNumber");
 const equals = document.querySelector("#equals");
 const cleaner = document.querySelector("#CButton");
 const decimal = document.querySelector("#decimalButton");
 const backspace = document.querySelector("#backspace");
 const sng = document.querySelector(".sng");
 const pi = document.querySelector("#pi");
+const percentage = document.querySelector("#percentage");
+const rnd = document.querySelector("#rounder");
+const rndCounter = document.querySelector("#rndCounter");
+const screenHistory = document.querySelector("#screenHistory");
+
+
 
 
 backspace.addEventListener("click", () => {
@@ -74,40 +117,88 @@ backspace.addEventListener("click", () => {
         numberA = "";
         screenNumber.textContent = 0;
     }
-});  
+}); 
+percentage.addEventListener("click", () => {
+    numberA = String(numberA);
+    numberB = String(numberB);
+    if(!numberA.includes("%") && operator === ""){
+            numberA = numberA + "%"; 
+            screenNumber.textContent = numberA;            
+    }
+    else if(operator !== "" && !numberB.includes("%")){
+            numberB = numberB + "%"; 
+            screenNumber.textContent = numberA + operator + numberB;            
+    }    
+}); 
+rnd.addEventListener("click", () => {
+    rounder = rounder + 1;
+    zeroCounter += "0"
+    if(rounder === 6){
+        rounder = 0;
+        zeroCounter = "."
+        rndCounter.textContent = "rnd: 0";
+    }
+    else rndCounter.textContent = `rnd: 0${zeroCounter}`
+
+});
 sng.addEventListener("click", () => {
     singular = true;
-}); 
+});
 decimal.addEventListener("click", () => {
-    if(numberA === ""){
-        numberA = "0.";
-        screenNumber.textContent = numberA;
+    numberA = String(numberA);
+    numberB = String(numberB);
+    if(!numberA.includes(".") && operator === ""){
+        if(numberA === "" && !singular){
+            numberA = "0.";
+            screenNumber.textContent = numberA;
+        }
+        else{
+            numberA = numberA + "."; 
+            screenNumber.textContent = numberA;            
+        }
+
     }
-    else if(numberB === ""){
-        numberB = "0.";
-        screenNumber.textContent = numberA + operator + numberB;
+    else if(!numberB.includes(".") && operator !== ""){
+        if(numberB === ""){
+            numberB = "0.";
+            screenNumber.textContent = numberA + operator + numberB;
+        }
+        else{
+            numberB = numberB + "."; 
+            screenNumber.textContent = numberA + operator + numberB;            
+        }
     }
 });  
 calcNumbers.forEach((number) => {
     number.addEventListener("click", () => {
+    numberA = String(numberA);
+    numberB = String(numberB);
+        if(number.textContent === "π"){
+            piNum = String(Math.PI.toFixed(rounder));
+            NumberFromKey = piNum;
+        }
+        else{
+            NumberFromKey = number.textContent;
+        }
+
         if(operator === ""){
-            if(numberA !== ""){
-                numberA = numberA + number.textContent; 
+            if(numberA !== "" && !numberA.includes("%")){
+                numberA = numberA + NumberFromKey; 
                 screenNumber.textContent = numberA;
             }
-            else{
+            else if(!numberA.includes("%")){
                 screenNumber.style.fontSize = "3rem";
-                numberA = number.textContent; 
+                numberA = NumberFromKey; 
                 screenNumber.textContent = numberA;
             }
         }
         else{
-            if(numberB !== ""){
-                numberB = numberB + number.textContent; 
+            if(numberB !== "" && !numberB.includes("%")){
+                numberB = numberB + NumberFromKey; 
                 screenNumber.textContent = numberA + operator + numberB;
             }
-            else{
-                numberB = number.textContent;
+            else if(!numberB.includes("%")){
+                numberB = NumberFromKey;
                 screenNumber.textContent = numberA + operator + numberB;
             }
         }
@@ -116,10 +207,20 @@ calcNumbers.forEach((number) => {
 calcOperators.forEach((opr) => {
     opr.addEventListener("click", () => {
         if(numberB !== ""){
+            screenHistory.textContent = numberA + operator + numberB;
             numberA = operate(operator, numberA, numberB);
+            if(numberA === "NaN"){
+                screenNumber.style.fontSize = "1.4rem";
+                screenNumber.textContent = "Error: malformed expression";
+                numberA = "";
+                numberB = "";
+                operator = "";
+            }
+            else{
             operator = `${opr.textContent}`;
             screenNumber.textContent = numberA + operator;
-            numberB = "";
+            numberB = ""; 
+            }  
         }
         else{
         operator = `${opr.textContent}`;
@@ -128,31 +229,65 @@ calcOperators.forEach((opr) => {
     });    
 });
 equals.addEventListener("click", () => {
-    if(!singular && (numberA === "" || numberB === "" || operator === "")){
+    screenHistory.textContent = numberA + operator + numberB;
+    numberA = String(numberA);
+    numberB = String(numberB);
+    let zeroA = numberA[0]
+    let zeroB = numberB[0]
+    if(numberA.includes("%")){
+        numberA = (numberA.slice(0, numberA.length - 1))/100;
+    }
+    if(numberB.includes("%")){
+        numberB = (numberB.slice(0, numberB.length - 1))/100;
+    }
+
+    if(!singular && (numberA === "" || numberB === "" || operator === "" || zeroA === "%" || zeroB === "%")){
+        if(numberA !== "" && zeroA !== "%" && zeroB !== "%"){
+            numberA = Number.parseFloat(numberA).toFixed(rounder);
+            screenNumber.textContent = numberA;
+        }
+        else{
         screenNumber.style.fontSize = "1.4rem";
         screenNumber.textContent = "Error: malformed expression";
         numberA = "";
         numberB = "";
         operator = "";
+        }
+    }
+    else if(singular && zeroB === "%"){
+        screenNumber.style.fontSize = "1.4rem";
+        screenNumber.textContent = "Error: malformed expression";
+        numberA = "";
+        numberB = "";
+        operator = "";        
     }
     else if(singular && numberA === "" && numberB !== ""){
         numberA = operate(operator, numberA, numberB);
+        numberA = removeZeros(numberA);
         screenNumber.textContent = numberA;
         operator = "";
         numberB = "";
         singular = false;
     }
     else{
-        if(operator === "/" && numberB === "0"){
+        if(operator === "÷" && numberB === "0"){
             screenNumber.style.fontSize = "1.4rem";
             screenNumber.textContent = "Error: division by 0 is not possible";
             numberA = "";
             numberB = "";
             operator = "";
         }
+        else if(operator === "mod" && numberB === "0"){
+            screenNumber.style.fontSize = "1.4rem";
+            screenNumber.textContent = "Error: modulo by 0 is not possible";
+            numberA = "";
+            numberB = "";
+            operator = "";
+        }
         else if(singular){
-            numberB = Number.parseFloat(squareRoot(numberB)).toFixed(2);
+            numberB = Number.parseFloat(squareRoot(numberB)).toFixed(rounder);
             numberA = operate("*", numberA, numberB);
+            numberA = removeZeros(numberA);
             screenNumber.textContent = numberA;
             operator = "";
             numberB = "";
@@ -160,6 +295,7 @@ equals.addEventListener("click", () => {
         }
         else{
             numberA = operate(operator, numberA, numberB);
+            numberA = removeZeros(numberA);
             screenNumber.textContent = numberA;
             operator = "";
             numberB = "";
@@ -171,16 +307,18 @@ cleaner.addEventListener("click", () => {
     numberA = "";
     numberB = "";
     operator = "";
+    singular = false;
     screenNumber.textContent = 0;
 });  
 
+let rounder = 2;
+let NumberFromKey = "";
 let singular = false;
 let numberA = "";
 let numberB = "";
 let operator = "";
-
-
-// overflowing numbers
-// nefunkncni dot
-// keyboard support
-// css mouseover color, sound, one =, proper color
+let piNum = String(Math.PI.toFixed(rounder));
+let zeroCounter = ".00"
+// pi overflow
+// overflowing numbers n font size scroller
+// keyboard support, sound
